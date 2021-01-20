f0bc302dbc365e24cd92ebdd6e472d21ae0d25a7
hf-hf <553527481@qq.com>
2021-01-20 13:47:12 +0800

    remove function

--- jd_nian.js
+++ jd_nian.js
@@ -93,7 +93,6 @@ async function jdNian() {
     await $.wait(1000)
     await doTask()
     await $.wait(2000)
-    await helpFriends()
     await $.wait(2000)
     await getHomeData(true)
     await showMsg()
